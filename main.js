cf876b093b719efffa04be5191eeb5ef704ee00d
diveet-123 <84632888+diveet-123@users.noreply.github.com>
2021-09-23 10:22:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function add_user()
+{
+    player1_name = document.getElementById("input_1").value;
+    player2_name = document.getElementById("input_2").value;
+   localStorage.setItem("player1_name" , player1_name);
+   localStorage.setItem("player2_name" , player2_name);
+   window.location = "next_page.html";
+}
